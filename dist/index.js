25ce582ce0a835b92aa35dc6537bb3a765a972c2
Andrey Sinyavskiy <sinerda@gmail.com>
2021-12-20 11:45:14 +0200

    'burger' menu. part 5

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,6 @@
 let ButtonMenu = document.querySelector('.button-burger-menu');
 let menu = document.querySelector('.adaptive-menu');
 ButtonMenu.addEventListener('click', function () {
-  menu.style.visibility = 'visible';
   menu.classList.toggle('active');
   ButtonMenu.classList.toggle('active');
 })
@@ -12,6 +11,19 @@ for (let elem of link) {
   elem.addEventListener('click', function () {
     menu.classList.toggle('active');
     ButtonMenu.classList.toggle('active');
-    menu.style.visibility = 'hidden';
   })
 }
+
+
+// function onClickClose(menu) { // вызвать в момент показа окна, где elem - окно
+//     function outsideClickListener(event) {
+//         if (!menu.contains(event.target) && isVisible(menu)) {  // проверяем, что клик не по элементу и элемент виден
+//              menu.style.display = 'none'; //скрыть
+//              document.removeEventListener('click', outsideClickListener);
+//         }
+//     }
+//     document.addEventListener('click', outsideClickListener)
+// }
+// function isVisible(menu) { //открыто ли условное окно
+//    return !!menu && !!( menu.offsetWidth || menu.offsetHeight || menu.getClientRects().length );
+// }
