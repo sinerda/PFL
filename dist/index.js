1f29529d9512b427bcb24dac8c4aee0a1ced0012
Andrey Sinyavskiy <sinerda@gmail.com>
2021-12-25 00:53:06 +0200

    "px-to-rem. part 1" and "opacity bug fix"

--- dist/index.js
+++ dist/index.js
@@ -21,46 +21,46 @@ $(window).scroll(function () {
 
     if (wScroll > $(".section-7").offset().top - ($(window).height() / 10)) {
       // Contact
-      $(".button-burger-menu").css("background-color", "#262626");
+      $(".button-burger-menu").css("background-color", "rgba(38, 38, 38, 0.7)");
       $(".button-burger-menu span").css("background-color", "#fff");
-      $(".adaptive-menu").css("background-color", "#262626");
+      $(".adaptive-menu").css("background-color", "rgba(38, 38, 38, 0.96)");
       $(".adaptive-menu a").css("color", "#fff");
     } else if (wScroll > $(".section-6").offset().top - ($(window).height() / 10)) {
       // Blog
-      $(".button-burger-menu").css("background-color", "#f72b3b");
+      $(".button-burger-menu").css("background-color", "rgba(247, 43, 60, 0.7)");
       $(".button-burger-menu span").css("background-color", "#fff");
       $(".adaptive-menu").css("background-color", "#fff");
-      $(".adaptive-menu a").css("color", "#f72b3b");
+      $(".adaptive-menu a").css("color", "rgba(247, 43, 60, 0.96)");
     } else if (wScroll > $(".section-5").offset().top - ($(window).height() / 10)) {
       // Subscribe
-      $(".button-burger-menu").css("background-color", "#96b92c");
+      $(".button-burger-menu").css("background-color", "rgba(150, 185, 44, 0.7)");
       $(".button-burger-menu span").css("background-color", "#000");
-      $(".adaptive-menu").css("background-color", "#96b92c");
+      $(".adaptive-menu").css("background-color", "rgba(150, 185, 44, 0.96)");
       $(".adaptive-menu a").css("color", "#000");
     } else if (wScroll > $(".section-4").offset().top - ($(window).height() / 10)) {
       // Gallery
-      $(".button-burger-menu").css("background-color", "#5585a2");
+      $(".button-burger-menu").css("background-color", "rgba(85, 132, 162, 0.7)");
       $(".button-burger-menu span").css("background-color", "#000");
-      $(".adaptive-menu").css("background-color", "#5585a2");
+      $(".adaptive-menu").css("background-color", "rgba(85, 132, 162, 0.96)");
       $(".adaptive-menu a").css("color", "#000");
     } else if (wScroll > $('.section-3').offset().top - ($(window).height() / 10)) {
       // Services
-      $(".button-burger-menu").css("background-color", "#ADB5BD");
+      $(".button-burger-menu").css("background-color", "rgba(173, 181, 189, 0.7)");
       $(".button-burger-menu span").css("background-color", "#000");
-      $(".adaptive-menu").css("background-color", "#ADB5BD");
+      $(".adaptive-menu").css("background-color", "rgba(173, 181, 189, 0.96)");
       $(".adaptive-menu a").css("color", "#000");
     } else if (wScroll > $('.section-2').offset().top - ($(window).height() / 10)) {
       // About Us
-      $(".button-burger-menu").css("background-color", "#fff");
+      $(".button-burger-menu").css("background-color", "rgba(255, 255, 255, 0.7)");
       $(".button-burger-menu span").css("background-color", "#000");
-      $(".adaptive-menu").css("background-color", "#fff");
+      $(".adaptive-menu").css("background-color", "rgba(255, 255, 255, 0.96)");
       $(".adaptive-menu a").css("color", "#000");
     }
     else {
       // Home
-      $(".button-burger-menu").css("background-color", "#ff6033");
+      $(".button-burger-menu").css("background-color", "rgba(255, 96, 51, 0.7)");
       $(".button-burger-menu span").css("background-color", "#fff");
-      $(".adaptive-menu").css("background-color", "#ff6033");
+      $(".adaptive-menu").css("background-color", "rgba(255, 96, 51, 0.96)");
       $(".adaptive-menu a").css("color", "#fff");
     }
   }
