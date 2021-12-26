b68405c550e1736d346479ef694ff79e6c4606fe
Andrey Sinyavskiy <sinerda@gmail.com>
2021-12-27 00:56:58 +0200

    Button-burger change color Update

--- dist/index.js
+++ dist/index.js
@@ -18,9 +18,9 @@ for (let elem of link) {
 $(window).scroll(function () {
   if ($(window).width() < 850) {
     // верхняя точка экрана в данный момент
-    let wScroll = $(this).scrollTop(),
-      // высота вьюпорта
-        wHeight = $(window).height() / 10;
+    let wScroll = $(this).scrollTop();
+    // высота вьюпорта
+    let wHeight = $(window).height() / 10;
     // 
     // Top of sections
     let TopContact = $(".section-7").offset().top,
@@ -31,79 +31,94 @@ $(window).scroll(function () {
         TopAboutUs = $('.section-2').offset().top,
         TopHome = $('.section-1').offset().top;
     //
-    // css celectors
+    // CSS celectors
     const Button = $(".button-burger-menu"),
+          ButtonActive = $(".button-burger-menu.active"),
           Button_span = $(".button-burger-menu span"),
           Menu = $(".adaptive-menu"),
           Menu_a = $(".adaptive-menu a");
     // 
     // Colors 
     const cButtonContact = "rgba(38, 38, 38, 0.7)",
+          cButtonActiveContact = "rgb(38, 38, 38)",
           cMenuContact = "rgba(38, 38, 38, 0.96)";
     
     const cButtonBlog = "rgba(247, 43, 60, 0.7)",
+          cButtonActiveBlog = "rgb(247, 43, 60)",
           cMenuBlog = "rgba(247, 43, 60, 0.96)";
     
     const cButtonSubscribe = "rgba(150, 185, 44, 0.7)",
+          cButtonActiveSubscribe = "rgb(150, 185, 44)",
           cMenuSubscribe = "rgba(150, 185, 44, 0.96)";
     
     const cButtonGallery = "rgba(85, 132, 162, 0.7)",
+          cButtonActiveGallery = "rgb(85, 132, 162)",
           cMenuGallery = "rgba(85, 132, 162, 0.96)";
     
     const cButtonServices = "rgba(173, 181, 189, 0.7)",
+          cButtonActiveServices = "rgb(173, 181, 189)",
           cMenuServices = "rgba(173, 181, 189, 0.96)";
     
     const cButtonAboutUs = "rgba(255, 255, 255, 0.7)",
+          cButtonActiveAboutUs = "rgb(255, 255, 255)",
           cMenuAboutUs = "rgba(255, 255, 255, 0.96)";
     
     const cButtonHome = "rgba(255, 96, 51, 0.7)",
+          cButtonActiveHome = "rgb(255, 96, 51)",
           cMenuHome = "rgba(255, 96, 51, 0.96)";
 
 
     if (wScroll > TopContact - wHeight) {
       Button.css("background-color", cButtonContact);
+      ButtonActive.css("background-color", cButtonActiveContact);
       Button_span.css("background-color", "#fff");
       Menu.css("background-color", cMenuContact);
       Menu_a.css("color", "#fff");
     }
     else if (wScroll > TopBlog - wHeight) {
       Button.css("background-color", cButtonBlog);
+      ButtonActive.css("background-color", cButtonActiveBlog);
       Button_span.css("background-color", "#fff");
       Menu.css("background-color", "#fff");
       Menu_a.css("color", cMenuBlog);
     }
     else if (wScroll > TopSubscribe - wHeight) {
       Button.css("background-color", cButtonSubscribe);
+      ButtonActive.css("background-color", cButtonActiveSubscribe);
       Button_span.css("background-color", "#000");
       Menu.css("background-color", cMenuSubscribe);
       Menu_a.css("color", "#000");
     }
     else if (wScroll > TopGallery - wHeight) {
       Button.css("background-color", cButtonGallery);
+      ButtonActive.css("background-color", cButtonActiveGallery);
       Button_span.css("background-color", "#000");
       Menu.css("background-color", cMenuGallery);
       Menu_a.css("color", "#000");
     }
     else if (wScroll > TopServices - wHeight) {
       Button.css("background-color", cButtonServices);
+      ButtonActive.css("background-color", cButtonActiveServices);
       Button_span.css("background-color", "#000");
       Menu.css("background-color", cMenuServices);
       Menu_a.css("color", "#000");
     }
     else if (wScroll > TopAboutUs - wHeight) {
       Button.css("background-color", cButtonAboutUs);
+      ButtonActive.css("background-color", cButtonActiveAboutUs);
       Button_span.css("background-color", "#000");
       Menu.css("background-color", cMenuAboutUs);
       Menu_a.css("color", "#000");
     }
     else {
       Button.css("background-color", cButtonHome);
+      ButtonActive.css("background-color", cButtonActiveHome);
       Button_span.css("background-color", "#fff");
       Menu.css("background-color", cMenuHome);
       Menu_a.css("color", "#fff");
     }
 
-    // Попытки развернуть алгоритм, чтобы визуально он выглядел более логично. Попытки на данный момент не увенчались успехом
+    // Попытки развернуть алгоритм задом наперёд, чтобы визуально он выглядел более логично. Попытки на данный момент не увенчались успехом
     // if (wScroll < wHeight) {
     //   Button.css("background-color", cButtonHome);
     //   Button_span.css("background-color", "#fff");
